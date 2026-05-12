5bf5da414970a8175300edb496a18d65e863cfb5
Happy <happylethabow@gmail.com>
2026-05-12 21:25:13 +0200

    Applied explicit casting to convert between data types

--- dataTypesCasting/Program.cs
+++ dataTypesCasting/Program.cs
@@ -5,17 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter the first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num1 = (int) Decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter the second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num2 = (int) Decimal.Parse(Console.ReadLine());   
 
             int sum = num1 + num2;
-            int quotient = num1 / num2;
+            double quotient = num1 / num2;
             
             Console.WriteLine($"The sum of the two numbers is: {sum}");
             
             Console.WriteLine($"The quotient of the two numbers is: {quotient}");
+
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+            Console.WriteLine($"{num1} / {num2} = {quotient}");
+
 
         }
     }
