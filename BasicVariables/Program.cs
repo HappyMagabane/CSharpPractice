112b2aa886b2e039b8cbfb071a0de073db98359d
Happy <happylethabow@gmail.com>
2026-05-12 12:14:39 +0200

    Created a program (BasicVariables) that declares and initializes variables of appropriate data types and specified values

---
+++ BasicVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace BasicVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 25; // myAge initializes with your age.
+            double myHeight = 1.75; // myHeight initializes with your height in meters.
+            bool IsPermentResident = true; // IsPermentResident initializes with true if you are a permanent resident, otherwise false.
+            string myNickname = "Lethabo"; // myNickname initializes with your nickname.
+            double myEtimatedNetWorth = 1000000.00; // myEtimatedNetWorth initializes with your estimated net worth.
+            string myLinkedIn = "https://www.linkedin.com/in/happymagabane/"; // myLinkedIn initializes with the URL of your LinkedIn profile.
+        }
+    }
+}
