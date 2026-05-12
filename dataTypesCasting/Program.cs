97457fd6d4a99ffaf56a4c4896de09df53dc7bf9
Happy <happylethabow@gmail.com>
2026-05-12 20:50:00 +0200

    Wrote a program (dataTypesCasting) that performs arithmetic operations on two numeric variables

---
+++ dataTypesCasting/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace dataTypesCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            int quotient = num1 / num2;
+            
+            Console.WriteLine($"The sum of the two numbers is: {sum}");
+            
+            Console.WriteLine($"The quotient of the two numbers is: {quotient}");
+
+        }
+    }
+}
