a5c467c3007d06a401342328bc55bbb03a9eab5a
Happy <happylethabow@gmail.com>
2026-05-14 14:11:02 +0200

    Wrote a program (CompareNumber) that compares two numbers entered by user, whether they are equal or not, using if- else statement

---
+++ CompareNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CompareNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 < num2)
+                Console.WriteLine("The first number is smaller than the second number.");
+            else if (num1 > num2)
+                Console.WriteLine("The first number is greater than the second number.");
+            else
+                Console.WriteLine("Both numbers are equal.");
+
+
+
+
+
+        }
+    }
+}
