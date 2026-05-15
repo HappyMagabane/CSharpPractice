c3d28530a14f60e49e95211004cb18580c12c29e
Happy <happylethabow@gmail.com>
2026-05-15 13:28:18 +0200

    Wrote a program (LogicInRange) that checks if a number entered by user is within a specified range, using if else statement

---
+++ LogicInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace LogicInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 100:");
+            int inputNumber= int .Parse(Console.ReadLine());
+
+            int lowerLimit = 1;
+            int upperLimit = 100;
+
+            if (inputNumber >= lowerLimit && inputNumber <= upperLimit)
+            {
+                Console.WriteLine("Number is between the range!");
+            }
+            else
+            {
+                Console.WriteLine("Number is out of the range!");
+            }
+
+
+
+              
+
+        }
+    }
+}
