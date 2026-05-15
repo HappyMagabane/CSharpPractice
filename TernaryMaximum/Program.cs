f375e61dec3bde90fbfc3ec61e3e9aa2d2a5dd74
Happy <happylethabow@gmail.com>
2026-05-15 14:51:10 +0200

    Wrote a program (TernaryMaximum)  using the ternary operator max if(condition, trueValue, falseValue)  to determine the maximum of two numbers (number1 and number2) entered by the user and display the result

---
+++ TernaryMaximum/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TernaryMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double max = (num1 > num2) ? num1 : num2;
+            Console.WriteLine($"The maximum of {num1} and {num2} is: {max}");
+        }
+    }
+}
