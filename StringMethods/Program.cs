5ded6a464ebcde23ac68979d66407a6ec03a2dc8
Happy <happylethabow@gmail.com>
2026-05-22 17:41:26 +0200

    Created a program (StringMethods) that has covered these methods in C#: String.Empty, Length, IsNullOrEmpty, ToUpper/ToLower,Replace, and Concat/Substring

---
+++ StringMethods/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleString = "Hello, Beautiful world. I am ";
+            string inputString = string.Empty;
+
+            int sampleLength = sampleString.Length;
+            int imputLength = inputString.Length;
+            bool isEmpty = string.IsNullOrEmpty(inputString);
+
+
+
+
+            Console.Write($"String: \"{sampleString}\"");
+            Console.Write($" Number of characters: {sampleLength}");
+            Console.WriteLine($" Is string empty? {isEmpty}");
+
+
+            Console.Write($"String: \"{inputString}\"");
+            Console.Write($" Number of characters: {imputLength}");
+            Console.WriteLine($" Is string empty? {isEmpty}");
+
+
+            Console.Write("Please enter a letter to search for in the string: ");
+            char searchChar = Console.ReadKey().KeyChar;
+
+            int firstOccurrence = sampleString.IndexOf(searchChar);
+            if (firstOccurrence != -1)
+            {
+                Console.WriteLine($"\nThe first occurrence of '{searchChar}' is at index: {firstOccurrence}");
+            }
+            else
+            {
+                Console.WriteLine($"\nThe character '{searchChar}' was not found in the string.");
+            }
+
+                string upperCaseString = sampleString.ToUpper();
+                Console.WriteLine($"Uppercase version of the string: {upperCaseString}");
+                string lowerCaseString = sampleString.ToLower();
+                Console.WriteLine($"Lowercase version of the string: {lowerCaseString}");
+
+            string replacedString = sampleString.Replace("world", "universe");
+            Console.WriteLine("Replaced string: " + replacedString);
+
+            Console.Write("Enter your name: ");
+            char[] nameChars = Console.ReadLine().ToCharArray();
+            Console.WriteLine($" {string.Join(", ", nameChars)} ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        }
+    }
+
