9e3e2df44dd00528d5669031ad9ad1a91dbeee89
Happy <happylethabow@gmail.com>
2026-05-25 17:55:18 +0200

    Created a program (AccessStringCharacters) that asks the user to enter a word. Display the first and last letter of the word.

---
+++ AccessStringCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AccessStringCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word: ");
+            string userWord = Console.ReadLine();
+
+            if (userWord.Length > 0)
+            {
+
+                char firstLetter = userWord[0];
+                char lastLetter = userWord[userWord.Length - 1];
+                Console.WriteLine($"First letter: {firstLetter}");
+                Console.WriteLine($"Last letter: {lastLetter}");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid word entered");
+
+            }
+        }
+    }
+}
