5d8e69fb0d87c22028d476259ca280d0cbf0b01c
Happy <happylethabow@gmail.com>
2026-05-27 20:08:12 +0200

    Wrote a program (CharacterMethods) which enters a single character and determine if it’s the letter, digit, white space, uppercase, lowercase, both letter and number

---
+++ CharacterMethods/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace CharacterMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a single character: ");
+            string userInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(userInput) || userInput.Length != 1)
+            {
+                Console.WriteLine("Error: You must enter exactly ONE character.");
+                return;
+            }
+            char ch = userInput[0];
+
+
+            if (char.IsLetter(ch))
+            {
+                Console.WriteLine(" It is a Letter.");
+            }
+
+            if (char.IsDigit(ch))
+            {
+                Console.WriteLine(" It is a Digit.");
+            }
+
+            if (char.IsWhiteSpace(ch))
+            {
+                Console.WriteLine(" It is a White Space.");
+            }
+
+  
+            if (char.IsUpper(ch))
+            {
+                Console.WriteLine(" It is Uppercase.");
+            }
+
+            
+            if (char.IsLower(ch))
+            {
+                Console.WriteLine(" It is Lowercase.");
+            }
+
+           
+            if (char.IsLetterOrDigit(ch))
+            {
+                Console.WriteLine("- It is an Alphanumeric character (Both letter and number classification).");
+            }
+        }
+    }
+}
+           
+       
+
+
+
+
+
+
+
