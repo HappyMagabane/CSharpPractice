9c1bcf1192385bd23db462e37de4b0de6bdcac8d
Happy <happylethabow@gmail.com>
2026-05-28 20:12:19 +0200

    Created a program (StringReversalProgram) that reverses a sentence entered by the user while preserving the order of words

---
+++ StringReversalProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace StringReversalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Word Order Reversal Program ---");
+            Console.WriteLine("Please enter a sentence:");
+
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("\n[!] You didn't enter any text to reverse.");
+                return;
+            }
+
+            
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+          
+            Array.Reverse(words);
+
+            string result = string.Join(" ", words);
+
+            
+            Console.WriteLine("\n==============================");
+            Console.WriteLine("REVERSED SENTENCE: ");
+         
+            Console.WriteLine(result);
+           
+        }
+    }
+}
+
+
