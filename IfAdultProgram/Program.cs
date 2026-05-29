f1e45a350999bfeb26019268799c34372366197e
Happy <happylethabow@gmail.com>
2026-05-29 08:05:51 +0200

    Created a program (IfAdultProgram) that asks the user to enter their age. If the age is greater than or equal to 18, display a message indicating they are an adult; otherwise, display a message indicating they are a minor

---
+++ IfAdultProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IfAdultProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int userInput= int.Parse(Console.ReadLine());
+
+            if (userInput >= 18)
+            {
+                Console.WriteLine("You are an adult!");
+            }
+            else
+            {
+                Console.WriteLine("You are a minor.");
+
+
+            }
+
+        }
+    }
+}
+
+
