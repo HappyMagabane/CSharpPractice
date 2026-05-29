813e5203e6d5c855379de276d7feb873d08458dc
Happy <happylethabow@gmail.com>
2026-05-29 08:14:37 +0200

    Created a program (IfPositiveNumber) that checks if a user-entered number is positive, negative, or zero using If-Else statements

---
+++ IfPositiveNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfPositiveNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else if (userInput > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+        }
+    }
+}
+
+
