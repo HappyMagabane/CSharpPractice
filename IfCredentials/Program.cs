597948e31281f93dca0500c4fa5dd58770e07d5e
Happy <happylethabow@gmail.com>
2026-05-29 09:52:24 +0200

    Created a program (IfCredentials) that asks the user to enter their username and password. If both are correct, display a welcome message; otherwise, display an error message

---
+++ IfCredentials/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IfCredentials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctUsername = "Admin";
+            string correctPassword = "MyPassword123";
+
+            Console.WriteLine("--- Login System ---");
+
+            Console.Write("Enter Username: ");
+            string inputUsername = Console.ReadLine();
+
+           
+            Console.Write("Enter Password: ");
+            string inputPassword = Console.ReadLine();
+
+        
+            if (string.Equals(inputUsername, correctUsername, StringComparison.OrdinalIgnoreCase) && inputPassword == correctPassword)
+            {
+             
+                Console.WriteLine("\n==============================");
+                Console.WriteLine("  Welcome! Login successful.  ");
+                Console.WriteLine("==============================");
+            }
+            else
+            {
+     
+                Console.WriteLine("\n==============================");
+                Console.WriteLine("  ERROR: Invalid Credentials! ");
+                
+            }
+        }
+    }
+}
