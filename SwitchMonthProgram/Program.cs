6a38038aa5e35d5ed3e8c3a844cf923db0c623a6
Happy <happylethabow@gmail.com>
2026-05-30 12:53:36 +0200

    Created a program (SwitchMonthProgram) that converts a user-entered month number (1 for January, 2 for February, etc.) into the corresponding month name using a Switch statement.

---
+++ SwitchMonthProgram/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace SwitchMonthProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Month Finder Tool ---");
+            Console.Write("Enter a number (1 for January, 2 for February, etc.): ");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int monthNumber))
+            {
+                string monthName;
+
+                switch (monthNumber)
+                {
+                    case 1:
+                        monthName = "January";
+                        break;
+                    case 2:
+                        monthName = "February";
+                        break;
+                    case 3:
+                        monthName = "March";
+                        break;
+                    case 4:
+                        monthName = "April";
+                        break;
+                    case 5:
+                        monthName = "May";
+                        break;
+                    case 6:
+                        monthName = "June";
+                        break;
+                    case 7:
+                        monthName = "July";
+                        break;
+                    case 8:
+                        monthName = "August";
+                        break;
+                    case 9:
+                        monthName = "September";
+                        break;
+                    case 10:
+                        monthName = "October";
+                        break;
+                    case 11:
+                        monthName = "November";
+                        break;
+                    case 12:
+                        monthName = "December";
+                        break;
+                    default:
+                        
+                        monthName = "Invalid month! Please enter a number between 1 and 12.";
+                        break;
+                }
+
+                
+                Console.WriteLine($"\nResult: {monthName}");
+            }
+            else
+            {
+             
+                Console.WriteLine("\n[!] Error: Please enter a valid numerical digit.");
+            }
+        }
+    }
+}
+   
