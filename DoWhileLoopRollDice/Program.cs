d529ddf301fdab97050f8e8bedd786cec4295d0a
Happy <happylethabow@gmail.com>
2026-06-10 23:16:00 +0200

    Creaated a program (DoWhileLoopRollDice) that simulates rolling a dice until a specific number (e.g., 6) is rolled. Count the number of rolls using a Do-While loop

---
+++ DoWhileLoopRollDice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DoWhileLoopRollDice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
