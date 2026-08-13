656e5eafcee631f5bf8604144f4332fc9946c390
Happy <happylethabow@gmail.com>
2026-08-13 19:11:21 +0200

    Created a program (PasswordRetryWhile), that prompts the user to enter a password. It keeps asking until the correct password is entered, using a While Loop.

--- DoWhileLoopRollDice/Program.cs
+++ DoWhileLoopRollDice/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=== Dice Roll Simulator ===");
+            Console.WriteLine("Rolling the die until we hit a 6...\n");
+
+            Random randomDice = new Random();
+            int rollCount = 0;
+            int currentRoll;
+
+     
+            do
+            {
+                
+                currentRoll = randomDice.Next(1, 7);
+
+                rollCount++;
+
+                
+                Console.WriteLine($"Roll #{rollCount}: You rolled a [{currentRoll}]");
+
+            } while (currentRoll != 6); 
+
+
+            Console.WriteLine("\n=============================================");
+            Console.WriteLine($"🎯 Success! It took a total of {rollCount} rolls to get a 6.");
+           
         }
     }
 }
---
+++ StarsTriangleNestedFor/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StarsTriangleNestedFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Nested Loop Star Triangle ===\n");
+
+            
+            int totalRows = 5;
+
+            
+            for (int row = 1; row <= totalRows; row++)
+            {
+               
+                for (int col = 1; col <= row; col++)
+                {
+                    
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
