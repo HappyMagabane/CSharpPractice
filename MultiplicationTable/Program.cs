3060b243b048f6f1d0d75416d8a9df67279c887d
Happy <happylethabow@gmail.com>
2026-08-20 21:21:05 +0200

    Created the program (MultiplicationTable) that displays a multiplication table (1 to 10) using nested For loops (for loop inside for loop). One for loop for row and another for column

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int col = 1; col <= 10; col++)
+                {
+                    int product = row * col;
+
+                    Console.Write($"{product}\t");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
