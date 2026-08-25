360b9882cb580492694c329ab8c94b2c02a0a42b
Happy <happylethabow@gmail.com>
2026-08-25 20:07:51 +0200

    Created a program (ArraySumAverage) that calculates the sum and average of elements in an Array of 5 numbers using a For loop. First enter the 5 numbers into the array then do the calculations treating it as you would a string of characters to access each number.

---
+++ ArraySumAverage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArraySumAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Array Sum and Average Calculator ===");
+
+            double[] numbers = new double[5];
+
+            Console.WriteLine("Please enter 5 numbers:\n");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number for index [{i}]: ");
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            double average = sum / numbers.Length;
+
+            Console.WriteLine($"Total Sum : {sum}");
+            Console.WriteLine($"Average   : {average:F2}");
+
+        }
+    }
+}
