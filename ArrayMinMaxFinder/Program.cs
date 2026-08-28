af2097149bb7eba0aa8f0e8763a9f090ac0be520
Happy <happylethabow@gmail.com>
2026-08-28 19:53:13 +0200

    Created the program (ArrayMinMaxFinder) that finds the maximum and minimum values in an array of numbers using a For loop. First enter the 5 numbers into the array then do the checking treating it as you would a string of characters to access each number

---
+++ ArrayMinMaxFinder/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ArrayMinMaxFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Array Min & Max Finder ===");
+
+          
+            double[] numbers = new double[5];
+
+          
+            Console.WriteLine("Please enter 5 numbers:\n");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number for index [{i}]: ");
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            
+            double min = numbers[0];
+            double max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                // Check for a new maximum value
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+
+               
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+     
+            Console.WriteLine($"Minimum Value: {min}");
+            Console.WriteLine($"Maximum Value: {max}");
+        
+        }
+    }
+}
