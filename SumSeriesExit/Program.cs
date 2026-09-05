2f9e8b8f46f81de003d3f854dc4b113f67005235
Happy <happylethabow@gmail.com>
2026-09-05 21:42:29 +0200

    ) that prompts the user to enter a series of numbers. Use a loop to calculate their sum. If the user enters a negative number, exit the loop and display the sum

---
+++ SumSeriesExit/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SumSeriesExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum Series Exit: ");
+            Console.WriteLine("Enter numbers one by one to add them to the total.");
+            Console.WriteLine("Enter any negative number to exit and see the sum.\n");
+
+            double sum = 0;
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+
+                if (double.TryParse(Console.ReadLine(), out double inputNumber))
+                {
+                  
+                    if (inputNumber < 0)
+                    {
+                        Console.WriteLine("Negative number detected. Exiting input loop...");
+                        break;
+                    }
+
+                    sum += inputNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid numerical value.\n");
+                }
+            }
+
+            Console.WriteLine($"Total Sum: {sum}");
+           
+        }
+    }
+}
+    
